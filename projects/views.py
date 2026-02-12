69e4504d25679fddbb50bea0dfb41fb92efb9ed8
Indranil Halder <indranilhalder.dev@gmail.com>
2026-02-12 12:52:57 +0530

    add templates
    Signed-off-by: Indranil Halder <indranilhalder.dev@gmail.com>

--- projects/views.py
+++ projects/views.py
@@ -4,8 +4,8 @@ from django.http import HttpResponse
 
 # Create your views here.
 def projects(request):
-    return HttpResponse("Here are our projects")
+    return render(request, "projects/projects.html")
 
 
 def project(request, pk):
-    return HttpResponse("Single Project" + " " + str(pk))
+    return render(request, "projects/single-project.html")
