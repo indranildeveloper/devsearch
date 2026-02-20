d3739412c3df828217ab2c4234d5fc23cf2a038c
Indranil Halder <indranilhalder.dev@gmail.com>
2026-02-20 20:58:10 +0530

    create project page
    Signed-off-by: Indranil Halder <indranilhalder.dev@gmail.com>

--- projects/forms.py
+++ projects/forms.py
@@ -1,4 +1,5 @@
 from django.forms import ModelForm
+from django import forms
 from .models import Project
 
 
@@ -13,3 +14,32 @@ class ProjectForm(ModelForm):
             "source_link",
             "tags",
         ]
+        widgets = {
+            "tags": forms.SelectMultiple(attrs={"class": "form-select", "size": "5"})
+        }
+
+    def __init__(self, *args, **kwargs):
+        super(ProjectForm, self).__init__(*args, **kwargs)
+        self.fields["title"].widget.attrs.update(
+            {"class": "form-control", "id": "Title", "placeholder": "Add title"}
+        )
+        self.fields["description"].widget.attrs.update(
+            {
+                "class": "form-control",
+                "id": "Description",
+                "placeholder": "Add description",
+            }
+        )
+        self.fields["featured_image"].widget.attrs.update(
+            {"class": "form-control", "id": "Featured image"}
+        )
+        self.fields["demo_link"].widget.attrs.update(
+            {"class": "form-control", "id": "Demo link", "placeholder": "Add demo link"}
+        )
+        self.fields["source_link"].widget.attrs.update(
+            {
+                "class": "form-control",
+                "id": "Source link",
+                "placeholder": "Add source link",
+            }
+        )
